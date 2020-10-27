a89b6768470d221a9ca2f303fbfc42e51c5be23a
Mohsen <muhsin86@hotmail.se>
2020-10-27 10:51:30 +0300

    Initial commit

---
+++ public/includes/footer.php
@@ -0,0 +1,13 @@
+<!-- FOOTER START -->
+<div class="footer">
+    <h2>WEBBUTVECKLING ||| PROJECT 2020, MOHSEN ALI MOHSEN</h2>
+</div>
+<!-- FOOTER END -->
+<!-- ALL CUSTOM SCRIPS -->
+<script src="../controller/courses.js"></script>
+<script src="../controller/projects.js"></script>
+<script src="../controller/works.js"></script>
+<!-- JAVASCRIPT END -->
+</body>
+</html>
+<!-- BODY AND HTML END TAGS -->
